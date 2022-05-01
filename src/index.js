b02393bae0e3c9dde008ad62c1d240b4f184cf63
Artyom <narshinovartyom@mail.ru>
2022-05-01 07:24:42 +0000

    swapi service

--- src/index.js
+++ src/index.js
@@ -2,7 +2,29 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 
+class SwapiService {
+    apiURL = "https://swapi.dev/api";
+
+    async getResorce(url) {
+        const response = await fetch(`${this.apiURL}${url}`);
+
+        if (!response.ok) {
+            throw new Error(
+                `Could not to get a data from ${url}` +
+                    `, recieved status: ${response.status}`
+            );
+        }
+        return response.json();
+    }
+
+    getAllPeople() {
+        const response = this.getResorce(`/people/`);
+        return response.results;
+    }
+    getPerson(id) {
+        return this.getResorce(`/people/${id}`);
+    }
+}
+
 const root = createRoot(document.getElementById("root"));
-root.render(
-    <h1>Hello world</h1>
-);
+root.render(<h1>Hello world</h1>);
