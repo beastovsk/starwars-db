50d2d7d0d971570e0a0f8a49dd82630822f5a802
Artyom <narshinovartyom@mail.ru>
2022-05-01 19:56:27 +0400

    visual application

--- src/index.js
+++ src/index.js
@@ -1,30 +1,8 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
+import App from './components/App/App'
 
-class SwapiService {
-    apiURL = "https://swapi.dev/api";
-
-    async getResorce(url) {
-        const response = await fetch(`${this.apiURL}${url}`);
-
-        if (!response.ok) {
-            throw new Error(
-                `Could not to get a data from ${url}` +
-                    `, recieved status: ${response.status}`
-            );
-        }
-        return response.json();
-    }
-
-    getAllPeople() {
-        const response = this.getResorce(`/people/`);
-        return response.results;
-    }
-    getPerson(id) {
-        return this.getResorce(`/people/${id}`);
-    }
-}
 
 const root = createRoot(document.getElementById("root"));
-root.render(<h1>Hello world</h1>);
+root.render(<App />);
