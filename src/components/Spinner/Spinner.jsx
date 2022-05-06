b52a0a27d9b2e454b07646d290da622651468b57
Artyom <narshinovartyom@mail.ru>
2022-05-06 08:13:39 +0400

    planet mapping, animation loading

---
+++ src/components/Spinner/Spinner.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+import "./Spinner.css";
+
+function Spinner() {
+    return (
+        <div class="loadingio-spinner-double-ring-t9c91jqylme">
+            <div class="ldio-dj7sje2ci2c">
+                <div></div>
+                <div></div>
+                <div>
+                    <div></div>
+                </div>
+                <div>
+                    <div></div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Spinner;
